224c9c8d17922ba2e1fbfd74a31227b447c41255
Victor Jerez <98106604+VictorJJerezm@users.noreply.github.com>
2025-10-18 07:47:28 -0600

    Prueba 2 18:10_2025

--- resources/views/panel/dashboard.blade.php
+++ resources/views/panel/dashboard.blade.php
@@ -12,6 +12,6 @@
   <a class="card" href="{{ route('proveedores.index') }}"><div class="card-body"><h3>Proveedores</h3><p class="muted">Contactar proveedores</p></div></a>
   <a class="card" href="{{ route('pedidos.index') }}"><div class="card-body"><h3>Pedidos</h3><p class="muted">Ver pedidos</p></div></a>
   <a class="card" href="{{ route('gestion.cotizaciones.index') }}"><div class="card-body"><h3>Cotizaciones</h3><p class="muted">Ver cotizaciones</p></div></a>
-  <a class="card" href="{{ route('reportes.index') }}"><div class="card-body"><h3>Reportes</h3><p class="muted">Metricas y Reportes</p></div></a>
+  <a class="card" href="{{ route('reportes.index') }}"><div class="card-body"><h3>Reportes</h3><p class="muted">Metricas y Reportes 12</p></div></a>
 </div>
 @endsection
